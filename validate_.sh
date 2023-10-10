3096565edde0114cb1235b1781d3dde5d4c7aea3
Douglas Martin <mdgeek1@gmail.com>
2023-10-10 17:19:35 -0400

    Use new webjar validator plugin.

--- validate_.sh
+++
@@ -1,9 +0,0 @@
-file_exists "import-map.json"
-file_contains "LICENSE" "MIT License"
-file_exists "README.md"
-
-file_exists "dist/lib/codemirror.js"
-dir_exists "dist/addon"
-dir_exists "dist/keymap"
-dir_exists "dist/mode"
-dir_exists "dist/theme"
